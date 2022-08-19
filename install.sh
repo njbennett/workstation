8ea0cc604f995cdec5ad10c115049814190be51a
Nat Bennett <natalie.j.bennett@gmail.com>
2022-08-18 18:49:57 -0700

    Use asdf to manage elixirs

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 brew bundle
 
 ruby-install ruby 2.7 --no-reinstall
+asdf plugin add elixir
+asdf install elixir 1.13.4-otp-25
+asdf global elixir 1.13.4-otp-25
 
 sed -i '' 's/plugins=.*/plugins=(git,z)/' ~/.zshrc
+grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.zshrc || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >> ~/.zshrc
