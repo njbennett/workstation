6ef438f2c08eced574b2e5b92bf49db097bcce03
Nat Bennett <natalie.j.bennett@gmail.com>
2022-08-18 18:57:10 -0700

    Specify zsh plugins properly

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ asdf plugin add elixir
 asdf install elixir 1.13.4-otp-25
 asdf global elixir 1.13.4-otp-25
 
-sed -i '' 's/plugins=.*/plugins=(git,z)/' ~/.zshrc
+sed -i '' 's/plugins=.*/plugins=(git z)/' ~/.zshrc
 grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.zshrc || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >> ~/.zshrc
