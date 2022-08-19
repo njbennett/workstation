ff472e5ddf919ddb29e2ececd58c2c7870f11409
Nat Bennett <natalie.j.bennett@gmail.com>
2022-08-19 09:29:52 -0700

    Install Erlang

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ brew bundle --force cleanup
 brew update
 
 ruby-install ruby 2.7 --no-reinstall
+asdf plugin add erlang
 asdf plugin add elixir
+
+asdf install erlang 25.0.4
+asdf global erlang 25.0.4
 asdf install elixir 1.13.4-otp-25
 asdf global elixir 1.13.4-otp-25
 
