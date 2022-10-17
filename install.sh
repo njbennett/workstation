a6c4511a132a36fced2738d73cb59db7e3854744
Nat Bennett <natalie.j.bennett@gmail.com>
2022-10-17 15:37:10 -0700

    Set up to run stable diffusion

--- install.sh
+++ install.sh
@@ -13,6 +13,10 @@ asdf global erlang 25.0.4
 asdf install elixir 1.13.4-otp-25
 asdf global elixir 1.13.4-otp-25
 
+# for stable diffusion
+asdf plugin-add python
+asdf install python 3.8.4
+
 sed -i '' 's/plugins=.*/plugins=(git z)/' ~/.zshrc
 grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.zshrc || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >> ~/.zshrc
 
