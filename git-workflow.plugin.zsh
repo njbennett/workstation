19c912155d7172c7192b8e355579384896b3a6fc
Nat Bennett <natalie.j.bennett@gmail.com>
2023-01-09 14:29:36 -0800

    Use dpup's git-workflow as a zsh plugin
    This is probably a goofy way to do this
    and there are maybe things wrong with the python dep install
    Uh, sorry, future me

---
+++ git-workflow.plugin.zsh
@@ -0,0 +1,4 @@
+# this plugin makes git-workflow commands available on the command-line
+# it assumes the required python environment is installed
+
+export PATH="$ZSH/custom/plugins/git-workflow:$PATH"
