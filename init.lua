85e092172bc759b0aac32f5930dbd2012cb4121c
Nat Bennett <natalie.j.bennett@gmail.com>
2023-07-31 11:22:44 -0700

    Add automatic formatting on save

--- init.lua
+++ init.lua
@@ -1,76 +1,85 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
---- { "elixir-editors/vim-elixir" },
-  { 'AlexvZyl/nordic.nvim' },
-  { 'neovim/nvim-lspconfig' },
---- make things a bit Luan's vim-ier
-  { 'ctrlpvim/ctrlp.vim' },
-  { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  },
+	--- { "elixir-editors/vim-elixir" },
+	{ 'AlexvZyl/nordic.nvim' },
+	{ 'neovim/nvim-lspconfig' },
+	--- make things a bit Luan's vim-ier
+	{ 'ctrlpvim/ctrlp.vim' },
+	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
 	{
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-   }
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.2',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	}
 }
 
 require("lazy").setup(plugins)
 
 --- lspconfig
-require'lspconfig'.lua_ls.setup{
-  settings = {
-    Lua = {
-      diagnostics = { globals = {'vim'} }
-    }
+require 'lspconfig'.lua_ls.setup {
+	settings = {
+		Lua = {
+			diagnostics = { globals = { 'vim' } }
+		}
 	}
 }
 
-require'lspconfig'.elmls.setup{}
+require 'lspconfig'.elmls.setup {}
 
 --- fancy syntax highlighting
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "css",
-    "elm",
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+		"css",
+		"elm",
 		"go",
-    "html",
+		"html",
 		"javascript",
-    "lua"
-  },
-  highlight = { enable = true},
+		"lua"
+	},
+	highlight = { enable = true },
 }
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf })
-  end,
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf })
+	end,
+})
+
+-- Format on writes
+vim.api.nvim_create_autocmd('BufWritePre', {
+	buffer = vim.fn.bufnr(),
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 3000 })
+	end,
 })
 
 --- keybindings
