d4a47162120385f501e7fb6171d6bb9e0a392434
Nat Bennett <nat@workgrounds.com>
2023-11-03 16:24:28 -0700

    Try using mason

--- init.lua
+++ init.lua
@@ -32,7 +32,9 @@ local plugins = {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
-		}
+		},
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" }
 	},
 	{
 		'nvim-telescope/telescope.nvim',
@@ -43,13 +45,18 @@ local plugins = {
 
 require("lazy").setup(plugins)
 
---- lspconfig
-local lspconfig = require 'lspconfig'
-
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+
+require("mason-lspconfig").setup {
+	ensure_installed = { "lua_ls", "rust_analyzer" },
+}
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
