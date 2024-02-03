e8d323d9aa21c566ee11c703140f5749435ac3fb
Nat Bennett <nat@macbook-pro.lan>
2024-02-02 17:40:02 -0800

    We install these things a different way now

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ brew bundle
 brew bundle --force cleanup
 brew update
 
-asdf plugin add ruby
-asdf install ruby 2.7.1
-asdf global ruby 2.7.1
-
 asdf plugin add erlang
 asdf plugin add elixir
 
@@ -20,8 +16,6 @@ asdf global elixir 1.14.3-otp-25
 asdf plugin add lua https://github.com/Stratus3D/asdf-lua.git
 asdf install lua 5.1.5
 asdf global lua 5.1.5
-
-./install-elixir-ls
 
 sed -i '' 's/plugins=.*/plugins=(git z)/' ~/.zshrc
 grep -qxF '. /usr/local/opt/asdf/libexec/asdf.sh' ~/.zshrc || echo '. /usr/local/opt/asdf/libexec/asdf.sh' >> ~/.zshrc
