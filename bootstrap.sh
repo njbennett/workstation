da1489a9b49bfeba3a78d16932a5d0b6611ddd0b
Nat Bennett <nat@simplermachines.com>
2024-02-06 19:00:14 -0800

    Complete shell installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/nat/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # install zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
