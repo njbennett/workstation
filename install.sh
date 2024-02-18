9578d1e4d4eac4418a69fe4d38e7ece38ceb3bd8
Nat Bennett <nat@simplermachines.com>
2024-02-17 20:21:20 -0800

    Don't fail when adding asdf plugins
    I don't like it either but add plugin exits non-zero if the plugin
    has already been added

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-set -eu
+set -eux
 
 ln -sf ~/workspace/workstation/.zshrc ~/.zshrc
 ln -sf ~/workspace/workstation/.gitconfig ~/.gitconfig
@@ -8,15 +8,17 @@ brew bundle
 brew bundle --force cleanup
 brew update
 
+set +e
 asdf plugin add erlang
 asdf plugin add elixir
+asdf plugin add lua https://github.com/Stratus3D/asdf-lua.git
+set -e
 
 asdf install erlang 25.2.3
 asdf global erlang 25.2.3
 asdf install elixir 1.14.3-otp-25
 asdf global elixir 1.14.3-otp-25
 
-asdf plugin add lua https://github.com/Stratus3D/asdf-lua.git
 asdf install lua 5.1.5
 asdf global lua 5.1.5
 
