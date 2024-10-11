783499561259022b772c83f1474e1f3c3726ec2d
Nat Bennett <nat@simplermachines.com>
2024-10-11 13:59:49 -0700

    Remove hardcoded usernames

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/nat/.zprofile
+username=$(ls -l /Users | awk '{print $9}' | grep v-)
+
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> "/Users/${username}/.zprofile"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install zsh
@@ -9,10 +11,3 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # install asdf
 git clone "https://github.com/asdf-vm/asdf.git" ~/.asdf --branch "v0.12.0"
-
-# Set the git username
-git config --global user.name "Nat Bennett"
-git config --global user.email nat@simplermachines.com
-
-# install Nix
-curl -L https://nixos.org/nix/install | sh
